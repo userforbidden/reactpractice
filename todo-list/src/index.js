b97940f5d7a9e35cc40dafb854d1be9392208055
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-02 18:56:57 -0500

    ReactDOM and JSX completed

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div><h1>Hello World</h1><p>This is a Paragraph</p></div>, document.getElementById("root"))
+function HelloWorld(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>This is a Paragraph</p>
+        </div>
+    )
+}
+
+function MyTodoApp() {
+    return (
+        <div><ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <HelloWorld/>, 
+    document.getElementById("root")
+    )
