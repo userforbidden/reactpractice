ecb3ce08cd3679173cf764127d57e28dd430f1f1
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-02 19:52:25 -0500

    Parent/child component 10. completed

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MyInfo from "./components/MyInfo"
+import App from "./components/App"
 
 ReactDOM.render(
-    <MyInfo/>, 
+    <App/>, 
     document.getElementById("root")
     )
