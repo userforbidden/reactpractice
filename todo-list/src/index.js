d2b63bb1c0b7c4480071859ebf0ff999097373b3
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-02 19:20:37 -0500

    completed 7.Functional Components

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -20,8 +20,30 @@ function MyTodoApp() {
         </div>
     )
 }
-
+//Objectives for Funtional Components practice 
+// 1. Setup the basic React Code from Scratch 
+// 2. Create a Fucntional component called MyInfo that return the following UI:
+    // a. An H1 with your name 
+    // b. A paragraph with a little blurb about yourself
+    // c. An ordered or Unordered list of the top 3 vacation spots you'd like to visit 
+// 3. Render an instance of that functional component to the browser 
+// Extra Challenge: learn on your own (Google!) how you can add some style to your page.
+function MyInfo(){
+    return (
+        <div>
+            <h1>Saranyan Senthivel</h1>
+            <p>I am a Software Engineer currently working with DXC Technology in New Orleans, Louisiana. I completed my Master's in Computer Science at University of New Orleans
+                Completed my Bachelor's from Dr. Mahalingam College of Engineering and technology, Pollachi, India (Anna University). My hobbies are Gardening, Maintaining a planted aquarium and wanderlust.
+            </p>
+            <ul >Vacation spots
+                <li>Paris</li>
+                <li>London</li>
+                <li>Newyork</li>
+            </ul>
+        </div>
+    )
+}
 ReactDOM.render(
-    <HelloWorld/>, 
+    <MyInfo/>, 
     document.getElementById("root")
     )
