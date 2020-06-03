c4eaf632622dbbbb23b5ead51938db9beb78a9c2
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-03 13:28:31 -0500

    15. completed Inline styles

--- todo-list/src/WishApp.js
+++ todo-list/src/WishApp.js
@@ -4,17 +4,25 @@ function WishApp(){
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay
-
-    if (hours < 12){
-        timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17){
-        timeOfDay = 'afternoon';
-    } else{
-        timeOfDay = 'night';
+    const styles = {
+        fontSize: 50
     }
     
+
+    if (hours < 12){
+        timeOfDay = "morning"
+        styles.color = "#04756F"
+    } else if (hours >= 12 && hours < 17){
+        timeOfDay = 'afternoon'
+        styles.color = "purple"
+    } else{
+        timeOfDay = 'night'
+        styles.color = "#D90000";
+    }
+
+    
     return(
-        <h1>Good {timeOfDay}!</h1>
+        <h1 style = {styles} >Good {timeOfDay}!</h1>
     )
 }
 
