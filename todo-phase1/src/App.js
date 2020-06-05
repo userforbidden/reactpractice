8333808365a8017448a1e20e83c236a90c0706c9
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-04 20:00:37 -0500

    24. Completed Class based components

--- todo-phase1/src/App.js
+++ todo-phase1/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
-import TodoHeader from './components/TodoHeader'
+//import TodoHeader from './components/TodoHeader'
 
-function App() {
-  return (
-    <div>
-      <TodoHeader />    
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <TodoHeader />    
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Saranyan Senthivel</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
