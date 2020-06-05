93ec3a43b78c05d1314f7bc80565a3f8a03e8d20
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-04 20:28:27 -0500

    25. Completed class-based components practice

--- todo-phase1/src/App.js
+++ todo-phase1/src/App.js
@@ -11,11 +11,49 @@ import './App.css';
 // }
 
 class App extends React.Component{
-  render(){
-    return (
+  render() {
+    return(
       <div>
-        <h1>Saranyan Senthivel</h1>
+          <Header username="Saranyan"/>
+          <Greeting />
       </div>
+    )
+  }
+}
+
+
+class Header extends React.Component{
+  render(){
+    return(
+      <header>
+          <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+
+class Greeting extends React.Component{
+  render(){
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+        fontSize: 20
+    }
+    if (hours < 12){
+        timeOfDay = "morning"
+        styles.color = "#04756F"
+    } else if (hours >= 12 && hours < 17){
+        timeOfDay = 'afternoon'
+        styles.color = "purple"
+    } else{
+        timeOfDay = 'night'
+        styles.color = "grey";
+    }
+
+    return(
+      <h2 style={styles}>Good {timeOfDay} to you, sir or madam</h2>
     )
   }
 }
