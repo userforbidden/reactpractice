f724f2a49bcadf5e98e8b81c5d532b7a491cd4fa
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-12 19:38:59 -0500

    passing arguments

--- simple-shopping-cart/src/components/counter.jsx
+++ simple-shopping-cart/src/components/counter.jsx
@@ -5,6 +5,10 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1", "tag2", "tag3"],
   };
+  /* constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  } */
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
@@ -16,13 +20,34 @@ class Counter extends Component {
       </ul>
     );
   }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => {
+            this.handleIncrement({ id: 1 });
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
